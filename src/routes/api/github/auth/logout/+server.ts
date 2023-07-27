a41a83c50f3764854f40484578582577a2b92d4c
George Ciubotaru <geeku96@gmail.com>
2023-07-27 16:24:25 +0300

    feat: delete token on logout

--- src/routes/api/github/auth/logout/+server.ts
+++ src/routes/api/github/auth/logout/+server.ts
@@ -1,8 +1,13 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
 import { names, serializeCookie } from '$lib/CookieManager';
+import app from '$lib/server/github';
 
 export const GET: RequestHandler = async ({ cookies }) => {
+    const accessToken = cookies.get(names.accessTokenCookieName);
+    if (accessToken) {
+        await app.oauth.deleteToken({ token: accessToken })
+    }
     cookies.delete(names.accessTokenCookieName, serializeCookie({
         expires: new Date(0)
     }))
