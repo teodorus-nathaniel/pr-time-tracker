c21f8a018f4d48607acd877b7590704aaf53977d
Sunday Power Inemesit <sundaypower9@gmail.com>
2023-08-08 13:30:14 +0100

    feat: add `Input` component (#64)
    * feat: resolve #62.
    
    * chore: remove comment.
    
    ---------
    
    Co-authored-by: George Ciubotaru <geeku96@gmail.com>

---
+++ src/routes/prs/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  /** internals */
+  import Toggle from '$lib/components/Toggle/index.svelte';
+  import Input from '$lib/components/Input/index.svelte';
+
+  /** vars */
+  let hours = '';
+</script>
+
+<Toggle leftButtonProps={{ text: 'Unsubmitted' }} rightButtonProps={{ text: 'Submitted' }} />
+<Toggle isReactionToggle />
+<Input
+  bind:value={hours}
+  onInput={(e) => {
+    console.log({ value: e.currentTarget.value });
+  }} />
+
+{hours}
