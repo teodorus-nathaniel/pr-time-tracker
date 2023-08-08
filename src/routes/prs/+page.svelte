dc18810764896627c88e6afbbbd28a7f5bb476d2
power-f-god <sundaypower9@gmail.com>
2023-08-08 17:59:44 +0100

    feat: Add header UI for (archived) PR's screen.

--- src/routes/prs/+page.svelte
+++ src/routes/prs/+page.svelte
@@ -5,7 +5,7 @@
   /** vars */
 </script>
 
-<main class="max-w-container m-auto py-4 md:py-8">
+<main class="max-w-container m-auto py-4 animate-fadeIn md:py-8">
   <ul class="grid gap-4 md:gap-8">
     <Card />
     <Card isReadonly />
