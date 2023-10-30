a17759f2f98b1b388a4ba5cba4c7b15ce9101eee
Sunday Power Inemesit <sundaypower9@gmail.com>
2023-10-30 14:06:59 +0100

    feat(pr): display contributors list. (#178)

--- src/lib/components/Avatar/index.svelte
+++ src/lib/components/Avatar/index.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
+  import type { IconProps } from '../types';
+
+  import Icon from '../Icon/index.svelte';
+
   /** props */
   export let url: string = '';
   export let alt: string = '';
   export let size: 'extra-small' | 'small' | 'medium' = 'small';
+  export let withIcon: IconProps | undefined = undefined;
 
   /** vars */
   let displayAvatarFallback = false || !url;
 
   /** props type */
-  type $$Props = { url?: string; alt?: string; size?: typeof size };
+  type $$Props = { url?: string; alt?: string; size?: typeof size; withIcon?: IconProps };
 </script>
 
 <span
@@ -16,16 +21,25 @@
     ? 'w-8 h-8'
     : size === 'extra-small'
     ? 'w-6 h-6'
-    : 'w-12 h-12'} bg-l3 rounded-full overflow-clip text-t3">
-  {#if !displayAvatarFallback}
-    <img
-      src={url}
-      {alt}
-      width={size === 'small' ? '32' : '48'}
-      height={size === 'small' ? '32' : '48'}
-      on:error={() => (displayAvatarFallback = true)}
-      class="object-cover object-center" />
-  {:else}
-    {(alt[0] || 'a').toUpperCase()}
+    : 'w-12 h-12'} relative text-t3">
+  <span class="inline-flex w-full h-full rounded-full overflow-clip items-center bg-l3">
+    {#if !displayAvatarFallback}
+      <img
+        src={url}
+        {alt}
+        width={size === 'small' ? '32' : '48'}
+        height={size === 'small' ? '32' : '48'}
+        on:error={() => (displayAvatarFallback = true)}
+        class="object-cover object-center" />
+    {:else}
+      {(alt[0] || 'a').toUpperCase()}
+    {/if}
+  </span>
+
+  {#if withIcon}
+    <span
+      class="inline-grid w-4 h-4 place-items-center bg-l3 border border-l3 rounded-full absolute -bottom-0.5 -right-2">
+      <Icon {...withIcon} class="{withIcon.class} w-full h-full" />
+    </span>
   {/if}
 </span>
--- src/routes/contributors/[id]/__components/ManagerPR.svelte
+++ src/routes/contributors/[id]/__components/ManagerPR.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-  import type { CardProps } from '../../../../lib/components/types';
-  import type { Experience } from '$lib/@types';
+  import type { CardProps } from '$lib/components/types';
+  import type { ContributorSchema, Experience, SubmissionSchema } from '$lib/@types';
 
   import Button from '$lib/components/Button/index.svelte';
+  import Avatar from '$lib/components/Avatar/index.svelte';
   import Icon from '$lib/components/Icon/index.svelte';
   import PR from '$lib/components/Card/PR.svelte';
+  import { createEffect } from '$lib/utils';
 
   import CostBreakdown from './CostBreakdown.svelte';
 
@@ -27,16 +29,26 @@
     hours?: number;
     cost: number | string;
   }> = [];
+  let owner: ContributorSchema;
+  let otherContributors: ContributorSchema[] = [];
+  const submissionsMap: Record<string, SubmissionSchema | undefined> = {};
+
+  const useSubmissionEffect = createEffect();
 
   /** react-ibles */
-  $: {
+  $: useSubmissionEffect(() => {
     totalCost = 0;
+    otherContributors = [];
     breakdown =
-      data.contributors?.map(({ id, rate, avatarUrl, name }) => {
+      data.contributors?.map((contributor) => {
+        const { id, rate, avatarUrl, name } = contributor;
         const submission = data.submissions?.find(({ owner_id }) => owner_id === id);
         const cost = Number(((submission?.hours || 0) * rate).toFixed(2));
 
         totalCost += cost;
+        if (name === data.owner) owner = contributor;
+        else otherContributors.push(contributor);
+        submissionsMap[name] = submission;
 
         return {
           experience: submission?.experience,
@@ -47,21 +59,47 @@
           cost
         };
       }) || [];
-  }
+  }, [data.submissions?.length]);
   $: openedAt = data.created_at ? new Date(data.created_at) : undefined;
 </script>
 
 <PR {isReadonly} {isAdmin} {loading} {data} {onSubmit}>
   <svelte:fragment slot="author-et-al">
-    <!-- <div class="flex gap-4 my-4 px-4 justify-between text-t3">
-      <div>
+    <div class="flex gap-4 my-4 px-0 justify-between text-t3">
+      <div class="flex gap-8">
         <span class="flex gap-1.5 flex-col max-w-content">
-          <span class="text-sm">Author:</span>
-          <Avatar url="" alt={data.owner} size="extra-small" />
+          <span class="text-sm">Owner:</span>
+          <Avatar
+            url={owner.avatarUrl}
+            alt={owner.name}
+            size="extra-small"
+            withIcon={{
+              name: 'check-circle',
+              class: submissionsMap[owner.name] ? 'text-accent2-default' : undefined
+            }} />
         </span>
+
+        <div class="flex gap-1.5 flex-col max-w-content">
+          <span class="text-sm">Participants:</span>
+
+          <div class="flex gap-3">
+            {#each otherContributors as { avatarUrl, name }}
+              <Avatar
+                url={avatarUrl}
+                alt={name}
+                size="extra-small"
+                withIcon={{
+                  name: 'check-circle',
+                  class: submissionsMap[name] ? 'text-accent2-default' : undefined
+                }} />
+            {:else}
+              --
+            {/each}
+          </div>
+        </div>
       </div>
 
-      <div>
+      <!-- <div>
         <span class="flex gap-1.5 text-sm flex-col max-w-content">
           <span class="">Opened:</span>
           <span class="text-t1">
@@ -70,8 +108,8 @@
               >@ {openedAt?.toLocaleTimeString().replace(/(\d\d:\d\d).*/, '$1') || '...'}</span>
           </span>
         </span>
-      </div>
-    </div> -->
+      </div> -->
+    </div>
   </svelte:fragment>
 
   <div class="grid grid-cols-4 border border-l4 rounded-2xl">
